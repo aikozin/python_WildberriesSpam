1ac831d90618f1efb4e52235301ebc7402231335
Kozin Alex <aikozin94@gmail.com>
2022-05-01 23:07:52 +0300

    ver 1.0

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import tkinter as tk
+import pyrebase
 
 import requests
 from seleniumwire import webdriver
@@ -13,6 +14,19 @@ search = True
 oldEvent = ''
 newEvent = ''
 userAgent = ''
+firebaseConfig = {
+  'apiKey': "AIzaSyB7fGTlgTMlPucaSi1Ta1VoiwBbOB2u7Yc",
+  'authDomain': "wildberriesspam-d3f26.firebaseapp.com",
+  'databaseURL': "https://wildberriesspam-d3f26-default-rtdb.asia-southeast1.firebasedatabase.app",
+  'projectId': "wildberriesspam-d3f26",
+  'storageBucket': "wildberriesspam-d3f26.appspot.com",
+  'messagingSenderId': "604940975627",
+  'appId': "1:604940975627:web:7a3e190c031a4a1372910e"
+}
+message = 'Здравствуйте! Мы компания, которая предоставляет услуги в сфере фулфилмента. Мы предложим Вам самую лучшую ' \
+          'цену на рынке! Если Вы заинтересованы, напишите обратное сообщение на номер WhatsApp: +7 977 447-88-22. ' \
+          'Если Вы дадите обратную связь в течение 3-х дней, мы предоставим Вам дополнительную СКИДКУ в размере 10%. ' \
+          'Спасибо за внимание! '
 
 
 def asyncUpdateCategoryInfo():
@@ -26,11 +40,11 @@ def asyncUpdateCategoryInfo():
             for item in driver.requests:
                 if 'fbrand' in item.url:
                     jsonObject = json.loads(item.response.body)
-                    global requestsJson
-                    requestsJson = jsonObject['data']['filters'][1]['items']
-                    userAgent = item.headers.get('user-agent')
-
-                    newCountBrands = len(requestsJson)
+                    if search:
+                        global requestsJson
+                        requestsJson = jsonObject['data']['filters'][1]['items']
+                        userAgent = item.headers.get('user-agent')
+                        newCountBrands = len(requestsJson)
             if oldCountBrands != newCountBrands:
                 oldCountBrands = newCountBrands
                 catalogTitle = driver.find_element(By.XPATH, "//h1[@class='catalog-title']").text
@@ -51,7 +65,6 @@ def start():
     txtEdit.insert(tk.END, 'Шаг 1: Войдите в учетную запись в открывшемся окне браузера Chrome \n\n')
     driver.get('https://www.wildberries.ru/security/login?returnUrl=https%3A%2F%2Fwww.wildberries.ru%2Fsecurity%2Flogin'
         '%3FreturnUrl%3Dhttps%253A%252F%252Fwww.wildberries.ru%252F')
-    # driver.get('https://www.wildberries.ru/catalog/krasota/aksessuary/aksessuary-dlya-makiyazha')
     txtEdit.insert(tk.END, 'Шаг 2: Выберите категорию товаров \n\n')
     thread1 = threading.Thread(target=asyncUpdateCategoryInfo)
     thread1.start()
@@ -85,14 +98,14 @@ def asyncSendMessages():
             url = 'https://questions.wildberries.ru/api/v1/question'
             headers = {
                 'User-Agent': userAgent,
-                'authorization': 'Bearer ' + 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlpkZUJNOG5xb0RCd3N4RkdnMjM5a1N4N1pZY2xncTZNWjVPSXVVRGdiSXciLCJ0eXAiOiJKV1QifQ.eyJleHAiOjE2NDk4NzMyMjEsImlhdCI6MTY0OTc4NjgyMSwicm9sZXMiOlsidXNlciJdLCJ1c2VyX2lkIjo1Mzk4NTM2N30.AUcxXvejd8J9WemfrucYKno0lO-fbYfKwPNgpbl4K06q_auvzIwaZy_HRhgrfpUhglHbllKGMj9oKsb_uwOIzdxpgE9_DxiMHOZVh4MYL69UKxx31W2bObFFmvZeC_i-1tSRMszsxQFWUM9eASj1jxW59UwF13dMZAH9y7jBRbijOncr2ZagL47b4gTqjlRN1Dg_inAGyDCtqKfAqDD0ZdmcSWaeP0RzkYQdnJH8b3Uqv62ICmIyiSkYROlFIhkXmP8T55KNVBDTYa_9rmpIbBOYjD3enUnuXYQArjnY5EhUDH5r0oaeaJ7Z-wvlbgvGvHEkkh-ZH6dceAaNEUpvkA'}
+                'authorization': 'Bearer ' + driver.get_cookie('access_token')['value']}
             response = requests.post(url,
                                      headers=headers,
                                      json={'imtId': imtId, 'text': message})
             if response.status_code == 200:
                 txtEdit.insert(tk.END, 'Успешно \n')
             else:
-                txtEdit.insert(tk.END, 'Непредвиденная ошибка \n')
+                txtEdit.insert(tk.END, 'Ошибка: ' + str(response.status_code) + '\n')
         except:
             pass
 
@@ -104,6 +117,15 @@ def asyncSendMessages():
 
 
 def send():
+    txtEdit.insert(tk.END, f'Получаем токен... \n')
+    driver.execute_script(
+        'window.open("https://www.wildberries.ru/gettoken", "new_window")')
+    time.sleep(1)
+    driver.switch_to.window(driver.window_handles[1])
+    time.sleep(1)
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+
     global userAgent
     global search
     global oldEvent
@@ -126,33 +148,33 @@ def waitEvent():
     oldEvent = newEvent
 
 
-message = 'Здравствуйте! Мы компания, которая предоставляет услуги в сфере фулфилмента. Мы предложим Вам самую лучшую ' \
-          'цену на рынке! Если Вы заинтересованы, напишите обратное сообщение на номер WhatsApp: +7 977 447-88-22. ' \
-          'Если Вы дадите обратную связь в течение 3-х дней, мы предоставим Вам дополнительную СКИДКУ в размере 10%. ' \
-          'Спасибо за внимание! '
-driver = webdriver.Chrome(service=Service("C:\\Users\\Alex\\Downloads\\chromedriver.exe"))
-driver.implicitly_wait(10)
-driver.maximize_window()
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
+isAllow = db.child('accessApp').get().val()['isAllow']
+if isAllow == 1:
+    driver = webdriver.Chrome(service=Service(".\\chromedriver.exe"))
+    driver.implicitly_wait(10)
+    driver.maximize_window()
 
-window = tk.Tk()
-window.title('Спам Wildberries')
-window.geometry('900x700')
-window.resizable(width=False, height=False)
-window.rowconfigure(0, minsize=200, weight=1)
-window.columnconfigure(1, weight=1)
+    window = tk.Tk()
+    window.title('Спам Wildberries')
+    window.geometry('900x700')
+    window.resizable(width=False, height=False)
+    window.rowconfigure(0, minsize=200, weight=1)
+    window.columnconfigure(1, weight=1)
 
-txtEdit = tk.Text(window, wrap=tk.CHAR)
-scroll = tk.Scrollbar(command=txtEdit.yview)
-frameButtons = tk.Frame(window)
-btnStart = tk.Button(frameButtons, text="Начать работу", command=start)
-btnSend = tk.Button(frameButtons, text="Начать рассылку", command=send)
+    txtEdit = tk.Text(window, wrap=tk.CHAR)
+    scroll = tk.Scrollbar(command=txtEdit.yview)
+    frameButtons = tk.Frame(window)
+    btnStart = tk.Button(frameButtons, text="Начать работу", command=start)
+    btnSend = tk.Button(frameButtons, text="Начать рассылку", command=send)
 
-btnStart.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btnSend.grid(row=1, column=0, sticky="ew", padx=5)
+    btnStart.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+    btnSend.grid(row=1, column=0, sticky="ew", padx=5)
 
-frameButtons.grid(row=0, column=0, sticky="ns")
-txtEdit.grid(row=0, column=1, sticky="nsew")
-scroll.grid(row=0, column=2, sticky="ns")
-txtEdit.config(yscrollcommand=scroll.set)
+    frameButtons.grid(row=0, column=0, sticky="ns")
+    txtEdit.grid(row=0, column=1, sticky="nsew")
+    scroll.grid(row=0, column=2, sticky="ns")
+    txtEdit.config(yscrollcommand=scroll.set)
 
-window.mainloop()
+    window.mainloop()
